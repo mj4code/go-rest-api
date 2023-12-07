bf5e07e886feaba3fca51c80a4a3a25267446a78
mritunjay.tiwari <mritunjay.tiwari@eagleview.com>
2023-12-07 16:36:53 +0530

    Added db connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/mj4code/go-rest-api/internal/db"
 )
 
 // Going to be responsible for instantiation
 // and startup of go application
 func Run() error {
 	fmt.Println("Starting the application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully connected and pinged database")
 	return nil
 }
 
