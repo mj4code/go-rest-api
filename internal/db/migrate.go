e066bf8689bc0a13b7e86a2d52cdb03728bfc7d3
mj4code <mritunjay4code@gmail.com>
2023-12-15 20:08:26 +0530

    Added migration layer

---
+++ internal/db/migrate.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+func (d *Database) MigrateDB() error {
+	log.Println("Migrating our database")
+
+	driver, err := postgres.WithInstance(d.Client.DB, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("could not create postgres driver: %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres",
+		driver,
+	)
+
+	if err != nil {
+		return fmt.Errorf("could not initialize migration instance: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("could not run up migrations: %w", err)
+	}
+
+	log.Println("Successfully migrated the database")
+	return nil
+}
